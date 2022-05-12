ebeddc6dd62f613258eee431475b014225219db4
Stephon Buckhanan <buckhananstephon@gmail.com>
2022-05-12 16:40:33 -0500

    added some console logs

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,8 +1,10 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
 let firstName = 'Stephon';
+console.log(firstName);
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Buckhanan';
+console.log(lastName);
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
 let fullName = `${firstName} ${lastName}`;
